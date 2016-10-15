72b0a70f2ab09b9a407b3b7034bd63d46798b337
jmo <jason.olson@mx.com>
2016-10-14 20:06:38 -0600

    fix eslint rules

--- express.js
+++ express.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(bodyParser.json());
 
-const AtriumClient = new Atrium.Client('d4df52dfdc4076f56665762e000a4bf9164e0049', '699413fc-73c0-43ae-b0dd-0abaedd0d654', Atrium.environments.local);
+const AtriumClient = new Atrium.Client('d4df52dfdc4076f56665762e000a4bf9164e0049', '699413fc-73c0-43ae-b0dd-0abaedd0d654', Atrium.environments.sand);
 
 Atrium.endpoints.forEach(endpoint => {
   app[endpoint.method](endpoint.url, (request, response) => {
